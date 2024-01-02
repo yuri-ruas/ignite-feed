91b8076d6a594d9ff13761760e16652c40b33b96
Yuri Ruas <yuriruas.dev@gmail.com>
2024-01-01 23:05:51 -0300

    application structure: css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header"
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Yuri Ruas"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta consequuntur architecto provident alias eius exercitationem hic tenetur, culpa sit, odit doloremque sequi sapiente cum adipisci ratione explicabo commodi esse? Obcaecati."

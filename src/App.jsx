85581e56edf727449df4f42bb44af2930d58ac3d
Yuri Ruas <yuriruas.dev@gmail.com>
2024-01-01 22:54:26 -0300

    getting started with react: properties

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Post } from "./Post"
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Yuri Ruas"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta consequuntur architecto provident alias eius exercitationem hic tenetur, culpa sit, odit doloremque sequi sapiente cum adipisci ratione explicabo commodi esse? Obcaecati."
+      />
+
+      <Post
+        author="Weslley Ruas"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }

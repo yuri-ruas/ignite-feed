13f173d3e86983629860bb9f81c2c388791af733
Yuri Ruas <yuriruas.dev@gmail.com>
2024-01-08 13:51:02 -0300

    react engines: communication between components

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,10 @@ import { Avatar } from './Avatar'
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/yuri-ruas.png" />
@@ -18,7 +21,7 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
